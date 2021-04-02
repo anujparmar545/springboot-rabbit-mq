ee8dc92fbaca91953d499bac72058eaec3011e1f
Anuj Parmar <anujparmar545@gmail.com>
2021-04-03 01:55:22 +0530

    first commit

---
+++ spring-rabbitmq-consumer/src/main/java/com/anuj/mq/MessageListener.java
@@ -0,0 +1,16 @@
+package com.anuj.mq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+	@RabbitListener(queues = MQConfig.QUEUE)
+	public void listener(CustomMessage message) {
+		
+		System.out.println("message"+message);
+		
+	}
+
+}
---
+++ spring-rabbitmq-producer/src/test/java/com/anuj/mq/ApringRabbitmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApringRabbitmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
